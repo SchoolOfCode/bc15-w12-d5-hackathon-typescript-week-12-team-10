1f622c8089ddc3d1caaa51138bf4e8cc03b5b316
Danny Ryan <dannykryan@gmail.com>
2023-11-24 11:31:44 +0000

    barbarian story added

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,44 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+// Define character choices as an enum
+enum CharacterChoice {
+  theif = 8,
+  Barbarian = 'barbarian',
+  Mage = 'mage'
+}
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+// Define scenarios and their corresponding messages
+const scenarios: Record<CharacterChoice, string> = {
+  [CharacterChoice.Barbarian]: 'You chose to be a barbarian. Unfortunately, you have met a fierce dragon and died.',
+  [CharacterChoice.Mage]: 'Congratulations! You chose to be a mage. You have successfully cast a spell and won the game!',
+};
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+function chooseAdventure(): void {
+  console.log('Welcome to the Choose Your Own Adventure Game!\n');
+
+  const rawCharacterChoice = window.prompt(
+    'Choose your character: Type "barbarian" or "mage"'
+  );
+
+  if (!rawCharacterChoice) {
+    console.log('Invalid choice. Refresh the page to try again.');
+    return;
+  }
+
+  // Convert the user input to a CharacterChoice
+  const characterChoice: CharacterChoice = rawCharacterChoice.toLowerCase() as CharacterChoice;
+
+  switch (characterChoice) {
+    case CharacterChoice.Barbarian:
+      console.log(scenarios[CharacterChoice.Barbarian]);
+      break;
+    case CharacterChoice.Mage:
+      console.log(scenarios[CharacterChoice.Mage]);
+      break;
+    default:
+      console.log('Invalid choice. Refresh the page to try again.');
+      break;
+  }
+
+  console.log('Thanks for playing! Goodbye.');
+}
+
+chooseAdventure();
